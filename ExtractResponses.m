c365f161ae179d2bfbc18b5f9aa0b1a7e83589ee
David <dshardman@outlook.com>
2022-03-11 10:14:30 +0000

    extract data

---
+++ ExtractResponses.m
@@ -0,0 +1,15 @@
+n = 10;
+
+positions = zeros(n, 3);
+responses = zeros(n, 192);
+
+for i = 0:n-1
+    position = readNPY('responses/position' + string(i) + '.npy');
+    response = readNPY('responses/response' + string(i) + '.npy');
+
+    positions(i+1, :) = position;
+    responses(i+1, :) = response;
+end
+
+npositions = normalize(positions);
+nresponses = normalize(responses);
