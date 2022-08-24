5cd643a4ad32bc71f97034b2532eb3524b4f0a53
David <dshardman@outlook.com>
2022-08-24 18:04:37 +0100

    ohms as raw unit

--- plotDamageDetect.m
+++ plotDamageDetect.m
@@ -5,7 +5,7 @@ load('Extracted/MultiTouch/superpose500.mat');
 % nb need to export in two halves to avoid matlab's multi-object workaround
 plot(0:8:8*(size(damage,1)-1), damage(:,91:end), 'LineWidth', 1);
 my_defaults([773.8000  457.8000  399.2000  281.6000]);
-ylabel('YUNIT');
+ylabel('Resistance (\Omega)');
 xlabel('Time (s)');
 xlim([0 6700]);
 
@@ -15,5 +15,5 @@ load('Extracted/Random/RandomSkin15k.mat');
 plot(responseups, 'LineWidth', 1);
 my_defaults(1000*[0.0826    0.5818    1.2648    0.2762]);
 set(gca, 'FontSize', 17)
-ylabel('YUNIT');
+ylabel('Resistance (\Omega)');
 xlabel('Presses');
