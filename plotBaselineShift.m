04bb21ed84cf9a5ee10fc55bbb5aa193f616965d
David <dshardman@outlook.com>
2022-09-02 18:53:44 +0100

    supplementary video made

--- plotBaselineShift.m
+++ plotBaselineShift.m
@@ -1,36 +1,39 @@
-%% Backprojection
-
-v = VideoWriter('baseshift');
-v.FrameRate = 30;
-open(v)
-load("Extracted/Random/Trained/AllTrained2k.mat");
-
-for i = 20:20:15000
-    [discrepancy, eitpred] = calcEIT(responseups(i,:), responseups(1,:), "bp");
-    for j = 1:20
-        output = netpredictions(net, responseups(1,:)-responseups(i+j-20,:),...
-            responsedowns(1:2000,:)-responseups(1:2000,:));
-        xy = (output(1:2) + 0.171/2)*1000*270./171;
-        scatter(xy(1), 270-xy(2), 100, 'k', 'filled');
-    end
-
-    writeVideo(v,getframe(gcf));
-    title(string(i));
-end
-close(v)
+% %% Backprojection
+% 
+% v = VideoWriter('baseshift');
+% v.FrameRate = 30;
+% open(v)
+% load("Extracted/Random/Trained/AllTrained2k.mat");
+% 
+% for i = 20:20:15000
+%     [discrepancy, eitpred] = calcEIT(responseups(i,:), responseups(1,:), "bp");
+%     for j = 1:20
+%         output = netpredictions(net, responseups(1,:)-responseups(i+j-20,:),...
+%             responsedowns(1:2000,:)-responseups(1:2000,:));
+%         xy = (output(1:2) + 0.171/2)*1000*270./171;
+%         scatter(xy(1), 270-xy(2), 100, 'k', 'filled');
+%     end
+% 
+%     writeVideo(v,getframe(gcf));
+%     title(string(i));
+% end
+% close(v)
 
 %% Sensitivity Maps
 
-v = VideoWriter('baseshiftsens10000');
+v = VideoWriter('baseshiftsens500_2');
 v.FrameRate = 30;
 open(v)
 for i = 20:20:15000
     if mod(i,1000) == 0
         i
     end
-    superposeMaps(10000, responsedowns, responseups, positions, i, 1);
-    writeVideo(v,getframe(gcf));
+    superposeMaps(500, responsedowns, responseups, positions, i, 1);
     title(string(i));
+    set(gcf, 'color', 'w');
+    set(gca, 'XTickLabels', [], 'YTickLabels', []);
+    set(gca,'xcolor','none', 'ycolor', 'none', 'FontSize', 15);
+    writeVideo(v,getframe(gcf));
     caxis([-5e5 10e5]);
     clf();
 end
--- superposeMaps.m
+++ superposeMaps.m
@@ -34,8 +34,8 @@ function [discrepancies, prediction] = superposeMaps(n, responsedowns, responseu
     else
         % single case, with plot
         assert(nargin == 6);
-        weights = responsedowns(plotnum,:) - responseups(plotnum,:);
-%         weights = responseups(plotnum,:) - responseups(1,:); % used for baseline comparisons
+%         weights = responsedowns(plotnum,:) - responseups(plotnum,:);
+        weights = responseups(plotnum,:) - responseups(1,:); % used for baseline comparisons
 %         weights = responseups(plotnum,:) - responseups(plotnum-20,:); % monitoring damage
         
         magnitudes = responsedowns(1:n,:) - responseups(1:n,:);
@@ -66,20 +66,20 @@ function [discrepancies, prediction] = superposeMaps(n, responsedowns, responseu
         contourf(xx,yy,value_interp, 20, 'LineStyle', 'none');
 
         hold on;
-        scatter(positions(plotnum,1), positions(plotnum,2), 50, 'k', 'filled');
+%         scatter(positions(plotnum,1), positions(plotnum,2), 50, 'k', 'filled');
         [~, ind] = sort(values, 'descend');
         prediction = [mean(positions(ind(1:m),1)) mean(positions(ind(1:m),2))];
         discrepancies = rssq(positions(plotnum,1:2)-prediction);
-        scatter(prediction(1), prediction(2), 50, 'r', 'filled');
+%         scatter(prediction(1), prediction(2), 50, 'r', 'filled');
         title(string(plotnum));
         xlim([-0.08 0.08]);
         ylim([-0.08 0.08]);
         axis square
-        set(gca,'visible','off');
+%         set(gca,'visible','off');
 % colorbar
 % caxis([-2e5 5e5]);
-        set(gcf, 'Position', [488.0000  579.4000  372.2000  278.6000]);
-        exportgraphics(gcf, string(plotnum) + "sens.png", 'BackgroundColor','w', 'Resolution',300);
+%         set(gcf, 'Position', [488.0000  579.4000  372.2000  278.6000]);
+%         exportgraphics(gcf, string(plotnum) + "sens.png", 'BackgroundColor','w', 'Resolution',300);
     end
 end
 
