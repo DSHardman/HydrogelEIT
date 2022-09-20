8399283ed083b7c36ea85a83cc5e5e66e1d53715
DSHardman <dshardman@outlook.com>
2022-09-20 12:28:43 +0100

    ohmc data collection

--- MembraneProbing.py
+++ MembraneProbing.py
@@ -29,7 +29,7 @@ urnie.set_tcp(wp.probing_tcp)
 # Connect to EIT board
 serial_handler = OpenEIT.backend.SerialHandler(queue.Queue())
 serial_handler.connect('COM4')
-serial_handler.setmode('d')
+serial_handler.setmode('c')
 serial_handler.start_recording()
 time.sleep(20)  # Give ample time to connect and start returning data
 
@@ -52,9 +52,9 @@ def pressrecord(x, y, depth, savestring):
     while serial_handler.updater != serial_state:
         pass  # wait for new set of readings to end
 
-    data = OpenEIT.backend.serialhandler.parse_any_line(serial_handler.raw_text, 'd')
+    data = OpenEIT.backend.serialhandler.parse_any_line(serial_handler.raw_text, 'c')
 
-    np.save('responses/multi/up' + savestring, data)
+    np.save('responses/ohmc8/up' + savestring, data)
 
     downpose = np.add(zeropose, [x, y, -depth, 0, 0, 0])
     # downpose = np.add(startingpose, [0, 0, -(0.01+depth), 0, 0, 0])
@@ -68,10 +68,10 @@ def pressrecord(x, y, depth, savestring):
     while serial_handler.updater != serial_state:
         pass  # wait for new set of readings to end
 
-    data = OpenEIT.backend.serialhandler.parse_any_line(serial_handler.raw_text, 'd')
+    data = OpenEIT.backend.serialhandler.parse_any_line(serial_handler.raw_text, 'c')
 
-    np.save('responses/multi/position' + savestring, xy)
-    np.save('responses/multi/down' + savestring, data)
+    np.save('responses/ohmc8/position' + savestring, xy)
+    np.save('responses/ohmc8/down' + savestring, data)
 
     urnie.movel(startingpose, acc=0.01, vel=0.01)
 
@@ -94,22 +94,22 @@ def pressrecord(x, y, depth, savestring):
 #     pressrecord(x, y, depth, '_doubleheal2_' + str(i))
 #     print(i)
 
-# for i in range(500):  # Randomise within circle
-#
-#     radius = 70
-#
-#     # Select from random distribution within circle
-#     rho = radius*2
-#     while rho > radius:
-#         x = (random.random()*2*radius-radius)/1000
-#         y = (random.random()*2*radius-radius)/1000
-#         rho = np.sqrt((x*1000)**2 + (y*1000)**2)
-#
-#     # depth = 0.01
-#     # depth = random.choice([0.005, 0.01, 0.015, 0.02])
-#     depth = random.choice([0.01, 0.015, 0.02])
-#     pressrecord(x, y, depth, '_superpose_' + str(i))
-#     print(i)
+for i in range(500):  # Randomise within circle
+
+    radius = 70
+
+    # Select from random distribution within circle
+    rho = radius*2
+    while rho > radius:
+        x = (random.random()*2*radius-radius)/1000
+        y = (random.random()*2*radius-radius)/1000
+        rho = np.sqrt((x*1000)**2 + (y*1000)**2)
+
+    # depth = 0.01
+    # depth = random.choice([0.005, 0.01, 0.015, 0.02])
+    depth = random.choice([0.01, 0.015, 0.02])
+    pressrecord(x, y, depth, '_ohmc_' + str(i))
+    print(i)
 
 # for i in range(10):  # 10 central repeats
 #     x = 0
@@ -117,19 +117,5 @@ def pressrecord(x, y, depth, savestring):
 #     depth = 0.025
 #     pressrecord(x, y, depth, '_repeathealdeep_' + str(i))
 #     print(i)
-
-# For multi-press configuration
-# pressrecord(0, 0, 0.01, 'multi_C_0_10')
-# pressrecord(0.7854, 0, 0.01, 'multi_C_45_10')
-# pressrecord(1.5708, 0, 0.01, 'multi_C_90_10')
-# pressrecord(3.1416, 0, 0.01, 'multi_C_180_10')
-#
-# pressrecord(0, 0, 0.015, 'multi_C_0_15')
-# pressrecord(0.7854, 0, 0.015, 'multi_C_45_15')
-# pressrecord(1.5708, 0, 0.015, 'multi_C_90_15')
-# pressrecord(3.1416, 0, 0.015, 'multi_C_180_15')
-
-# pressrecord(0, 0, 0.01, 'multi_C_cent_10')
-# pressrecord(0, 0, 0.015, 'multi_C_cent_15')
 
 urnie.close()
